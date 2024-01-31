96b9efcef3daf33912e01d71c98ffab1794455d9
Ben Collins <ben.collins@antithesis.com>
2024-01-30 22:34:37 -0500

    Docs for assert package

--- assert/assert.go
+++ assert/assert.go
@@ -1,3 +1,11 @@
+// Package assert allows callers to configure test oracles for the [Antithesis testing platform].
+//
+// For full functionality, code should be indexed by the exigen command
+// so that Antithesis can know what invocations to expect. This is needed
+// for Always, Sometime, and Reachable. It will make reporting about
+// Unreachable and AlwaysOrUnreachable more understandable.
+//
+// [Antithesis testing platform]: https://antithesis.com
 package assert
 
 import (
@@ -32,48 +40,70 @@ const was_hit = true
 const must_be_hit = true
 const optionally_hit = false
 const expecting_true = true
-const expecting_false = false
 
 const universal_test = "every"
 const existential_test = "some"
 const reachability_test = "none"
 
-// Always asserts that when this is evaluated
-// the condition will always be true, and that this is evaluated at least once.
-func Always(text string, cond bool, values map[string]any, options ...string) {
+// Assert that condition is true one or more times during a test. Callers of
+// `Always` can see failures in two cases:
+// 1. If this function is ever invoked with a `false` for the conditional or
+// 2. If an "indexed" invocation of Always is not covered at least once.
+// message will be used as a display name in reporting and should therefore be
+// useful to a broad audience. The map of values is used to supply context useful
+// for understanding the reason that condition had the value it did. For instance,
+// in an asertion that x > 5, it could be helpful to send the value of x so failing
+// cases can be better understood.
+func Always(message string, condition bool, values map[string]any, options ...string) {
 	location_info := newLocationInfo(offsetAPICaller)
-	assertImpl(text, cond, values, location_info, was_hit, must_be_hit, expecting_true, universal_test, options...)
+	assertImpl(message, condition, values, location_info, was_hit, must_be_hit, expecting_true, universal_test, options...)
 }
 
-// AlwaysOrUnreachable asserts that when this is evaluated
-// the condition will always be true, or that this is never reaached and evaluated.
-func AlwaysOrUnreachable(text string, cond bool, values map[string]any, options ...string) {
+// Assert that condition is true if it is ever evaluated. Callers will not
+// see a failure in their test if the condition is never evaluated.
+// message will be used as a display name in reporting and should therefore be
+// useful to a broad audience. The map of values is used to supply context useful
+// for understanding the reason that condition had the value it did. For instance,
+// in an asertion that x > 5, it could be helpful to send the value of x so failing
+// cases can be better understood.
+func AlwaysOrUnreachable(message string, condition bool, values map[string]any, options ...string) {
 	location_info := newLocationInfo(offsetAPICaller)
-	assertImpl(text, cond, values, location_info, was_hit, optionally_hit, expecting_true, universal_test, options...)
+	assertImpl(message, condition, values, location_info, was_hit, optionally_hit, expecting_true, universal_test, options...)
 }
 
-// Sometimes asserts that when this is evaluated
-// the condition will sometimes be true, and that this is evaluated at least once.
-func Sometimes(text string, cond bool, values map[string]any, options ...string) {
+// Assert that condition is true at least once in a test. Callers that invoke Sometimes will
+// only see an error if that particualr invocation is neven called with condtition true.
+// message will be used as a display name in reporting and should therefore be
+// useful to a broad audience. The map of values is used to supply context useful
+// for understanding the reason that condition had the value it did. For instance,
+// in an asertion that x > 5, it could be helpful to send the value of x so failing
+// cases can be better understood.
+func Sometimes(message string, condition bool, values map[string]any, options ...string) {
 	location_info := newLocationInfo(offsetAPICaller)
-	assertImpl(text, cond, values, location_info, was_hit, must_be_hit, expecting_true, existential_test, options...)
+	assertImpl(message, condition, values, location_info, was_hit, must_be_hit, expecting_true, existential_test, options...)
 }
 
-// Unreachable asserts that this is never evaluated.
-// This assertion will fail if it is evaluated.
-func Unreachable(text string, values map[string]any, options ...string) {
+// Assert that some path of code is not taken. A failure will be raised if this
+// function is ever called.
+// message will be used as a display name in reporting and should therefore be
+// useful to a broad audience. The map of values is used to supply context useful
+// for understanding the reason that this code path was taken.
+func Unreachable(message string, values map[string]any, options ...string) {
 	location_info := newLocationInfo(offsetAPICaller)
-	assertImpl(text, true, values, location_info, was_hit, optionally_hit, expecting_true, reachability_test, options...)
+	assertImpl(message, true, values, location_info, was_hit, optionally_hit, expecting_true, reachability_test, options...)
 }
 
-// Reachable asserts that this is evaluated at least once.
-// This assertion will fail if it is not evaluated, and otherwise will pass.
-func Reachable(text string, values map[string]any, options ...string) {
+// Assert that some path of code is tested. If any call to Reachable is not
+// invoked during the course of a test a failure will be noted.
+// message will be used as a display name in reporting and should therefore be
+// useful to a broad audience. The map of values is used to supply context useful
+// for understanding the reason that this code path was taken.
+func Reachable(message string, values map[string]any, options ...string) {
 	location_info := newLocationInfo(offsetAPICaller)
-	assertImpl(text, true, values, location_info, was_hit, must_be_hit, expecting_true, reachability_test, options...)
+	assertImpl(message, true, values, location_info, was_hit, must_be_hit, expecting_true, reachability_test, options...)
 }
 
-func assertImpl(text string, cond bool, values map[string]any, loc *locationInfo, hit bool, must_hit bool, expecting bool, assert_type string, options ...string) {
+func assertImpl(message string, cond bool, values map[string]any, loc *locationInfo, hit bool, must_hit bool, expecting bool, assert_type string, options ...string) {
 	message_key := makeKey(loc)
 	tracker_entry := assert_tracker.get_tracker_entry(message_key)
 
@@ -83,7 +113,7 @@ func assertImpl(text string, cond bool, values map[string]any, loc *locationInfo
 		AssertType: assert_type,
 		Expecting:  expecting,
 		Category:   "",
-		Message:    text,
+		Message:    message,
 		Condition:  cond,
 		Id:         message_key,
 		Location:   loc,
