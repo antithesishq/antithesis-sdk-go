07e96232999880df1bb8b80d3b1e45b89b7ee4f8
Ben Collins <ben.collins@antithesis.com>
2024-01-30 22:54:06 -0500

    Docs for lifecycle package

--- lifecycle/lifecycle.go
+++ lifecycle/lifecycle.go
@@ -1,11 +1,19 @@
+// Package lifecycle allows callers control of the [Antithesis environment].
+//
+// Callers running outside of the Antithesis environment will see no effect
+// from these calls. Optionally, calls will log their operations to the file
+// pointed to in the environment variable ANTITHESIS_SDK_LOCAL_OUTPUT, if
+// it is defined.
+//
+// [Antithesis testing platform]: https://antithesis.com
 package lifecycle
 
 import (
 	"github.com/antithesishq/antithesis-sdk-go/internal"
 )
 
-// SetupComplet indicates that the system under test
-// is ready for testing.
+// To be called once system setup is complete and the system is ready for
+// exploration.
 func SetupComplete() {
 	internal.Json_data(map[string]string{"setup_status": "complete"})
 }
