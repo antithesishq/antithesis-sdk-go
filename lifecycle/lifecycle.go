5f1676ce4bde099f01ae1cb8af48dab80fbc7ac9
Will Wilson <will@antithesis.com>
2024-02-03 11:11:29 -0500

    Edits to inline package documentation.

--- lifecycle/lifecycle.go
+++ lifecycle/lifecycle.go
@@ -1,19 +1,20 @@
-// Package lifecycle allows callers control of the [Antithesis environment].
+// This package is part of the [Antithesis Go SDK], which enables Go applications to integrate with the [Antithesis platform].
 //
-// Callers running outside of the Antithesis environment will see no effect
-// from these calls. Optionally, calls will log their operations to the file
-// pointed to in the environment variable ANTITHESIS_SDK_LOCAL_OUTPUT, if
-// it is defined.
+// The lifecycle package contains functions which inform the Antithesis environment that particular test phases or milestones have been reached. 
 //
-// [Antithesis testing platform]: https://antithesis.com
+// [Antithesis Go SDK]: https://antithesis.com/docs/using_antithesis/sdk/go_sdk.html 
+// [Antithesis platform]: https://antithesis.com
 package lifecycle
 
 import (
 	"github.com/antithesishq/antithesis-sdk-go/internal"
 )
 
-// To be called once system setup is complete and the system is ready for
-// exploration.
+// Call this function when your system and workload are fully initialized. After this function is called, the Antithesis environment will take a snapshot of your system and begin [injecting faults]. 
+//
+// Calling this function multiple times, or from multiple processes, will have no effect. Antithesis will treat the first time any process called this function as the moment that the setup was completed. 
+//
+// [injecting faults]: https://antithesis.com/docs/applications/reliability/fault_injection.html
 func SetupComplete() {
 	internal.Json_data(map[string]string{"setup_status": "complete"})
 }
