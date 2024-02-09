10c7ff6c43e0d52d1bf4e29f8d741c82152c7551
Paul Herzog <paul.herzog@antithesis.com>
2024-02-09 16:50:53 -0500

    WIP - Incorporating generator files to the instrumentor

--- tools/antithesis-go-instrumentor/main.go
+++ tools/antithesis-go-instrumentor/main.go
@@ -16,8 +16,6 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
-
-	// "github.com/golang/glog"
 )
 
 //go:embed version.txt
@@ -46,20 +44,17 @@ func verbose_level(v int) bool {
 // skipping any files or directories specified in exclusions.
 func FindSourceCode(inputDirectory string, exclusions map[string]bool) []string {
 	paths := []string{}
-	// glog.Infof("Scanning %s recursively for .go source", inputDirectory)
 	logger.Printf("Scanning %s recursively for .go source", inputDirectory)
 	// Files are read in lexical order, i.e. we can later deterministically
 	// hash their content: https://pkg.go.dev/path/filepath#WalkDir
 	err := filepath.WalkDir(inputDirectory,
 		func(path string, info fs.DirEntry, err error) error {
 			if err != nil {
-				// glog.Errorf("Error %v in directory %s; skipping", err, path)
 				logger.Printf("\u274c Error %v in directory %s; skipping", err, path)
 				return err
 			}
 
 			if b := filepath.Base(path); strings.HasPrefix(b, ".") {
-				// glog.V(2).Infof("Skipping %s", path)
                 if verbose_level(2) {
                     logger.Printf("Skipping %s", path)
                 }
@@ -71,11 +66,9 @@ func FindSourceCode(inputDirectory string, exclusions map[string]bool) []string 
 
 			if exclusions[path] {
 				if info.IsDir() {
-					// glog.Infof("Skipping excluded directory %s and its children", path)
 					logger.Printf("Skipping excluded directory %s and its children", path)
 					return fs.SkipDir
 				}
-				// glog.Infof("Skipping excluded file %s", path)
 				logger.Printf("Skipping excluded file %s", path)
 				return nil
 			}
@@ -87,13 +80,11 @@ func FindSourceCode(inputDirectory string, exclusions map[string]bool) []string 
 			}
 			// This is the mandatory format of unit test file names.
 			if strings.HasSuffix(path, "_test.go") {
-				// glog.V(2).Infof("Skipping test file %s", path)
                 if verbose_level(2) {
                     logger.Printf("Skipping test file %s", path)
                 }
 				return nil
 			} else if strings.HasSuffix(path, ".pb.go") {
-				// glog.V(1).Infof("Skipping generated file %s", path)
                 if verbose_level(1) {
                     logger.Printf("Skipping generated file %s", path)
                 }
@@ -106,7 +97,6 @@ func FindSourceCode(inputDirectory string, exclusions map[string]bool) []string 
 		})
 
 	if err != nil {
-		// glog.Fatalf("Error walking input directory %s: %v", inputDirectory, err)
 		logger.Fatalf("Error walking input directory %s: %v", inputDirectory, err)
 	}
 
@@ -121,7 +111,6 @@ func HashFileContent(paths []string) string {
 	for _, path := range paths {
 		bytes, err := ioutil.ReadFile(path)
 		if err != nil {
-			// glog.Fatalf("Error reading file %s: %v", path, err)
 			logger.Fatalf("Error reading file %s: %v", path, err)
 		}
 		hasher.Write(bytes)
@@ -133,7 +122,6 @@ func HashFileContent(paths []string) string {
 func validateInputAsModule(path string) {
 	moduleFile := filepath.Join(path, "go.mod")
 	if _, err := os.ReadFile(moduleFile); err != nil {
-		// glog.Fatalf("There was no readable go.mod file at %s: %v", path, err)
 		logger.Fatalf("There was no readable go.mod file at %s: %v", path, err)
 	}
 }
@@ -142,7 +130,6 @@ func validateAntithesisModule(path string) {
 	antithesisModuleFile := filepath.Join(path, "go.mod")
 	file, err := os.Open(antithesisModuleFile)
 	if err != nil {
-		// glog.Fatalf("There was no readable go.mod file at %s: %v", path, err)
 		logger.Fatalf("There was no readable go.mod file at %s: %v", path, err)
 	}
 	defer file.Close()
@@ -150,22 +137,18 @@ func validateAntithesisModule(path string) {
 	scanner := bufio.NewScanner(file)
 	scanner.Scan()
 	if err := scanner.Err(); err != nil {
-		// glog.Fatalf("There was no readable go.mod file at %s: %v", path, err)
 		logger.Fatalf("There was no readable go.mod file at %s: %v", path, err)
 	}
 	if scanner.Text() != "module antithesis.com/go/instrumentation" {
-		// glog.Fatalf("%s does not appear to be the go.mod for the Antithesis wrapper", antithesisModuleFile)
 		logger.Fatalf("%s does not appear to be the go.mod for the Antithesis wrapper", antithesisModuleFile)
 	}
 }
 
 func verifyGoOnPath() {
-	// glog.Info("Confirming that go is on $PATH...")
 	logger.Printf("Confirming that go is on $PATH...")
 	cmd := exec.Command("go", "version")
 	_, err := cmd.Output()
 	if err != nil {
-		// glog.Fatalf("%v", err)
 		logger.Fatalf("%v", err)
 	}
 }
@@ -177,12 +160,9 @@ func copyRecursiveNoClobber(from, to string) {
 	cmd := exec.Command("bash", "-c", commandLine)
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
-	// glog.Infof("%s", commandLine)
 	logger.Printf("%s", commandLine)
 	err := cmd.Run()
 	if err != nil {
-		// TODO Do something with STDERR?
-		// glog.Fatalf("%v", err)
 		logger.Fatalf("%v", err)
 	}
 }
@@ -191,12 +171,10 @@ func addDependencies(customerInputDirectory, antithesisOutputDirectory, customer
 	commandLine := fmt.Sprintf("(cd %s; go mod edit -require=antithesis.com/go/instrumentation@v1.0.0 -replace antithesis.com/go/instrumentation=%s -print > %s/go.mod)", customerInputDirectory, antithesisOutputDirectory, customerOutputDirectory)
 
 	cmd := exec.Command("bash", "-c", commandLine)
-	// glog.Infof("%s", commandLine)
 	logger.Printf("%s", commandLine)
 	_, err := cmd.Output()
 	if err != nil {
 		// Errors here are pretty mysterious.
-		// glog.Fatalf("%v", err)
 		logger.Fatalf("%v", err)
 	}
 }
@@ -205,14 +183,12 @@ func writeInstrumentedSource(source, path string) error {
 	// Any errors here are fatal anyway, so I'm not checking.
 	f, e := os.Create(path)
 	if e != nil {
-		// glog.Warningf("could not create %s", path)
 		logger.Printf("\u261d could not create %s", path)
 		return e
 	}
 	defer f.Close()
 	_, e = f.WriteString(source)
 	if e != nil {
-		// glog.Warningf("Could not write instrumented source to %s", path)
 		logger.Printf("\u261d Could not write instrumented source to %s", path)
 		return e
 	}
@@ -233,18 +209,18 @@ func main() {
 		fmt.Println(versionString)
 		os.Exit(0)
 	}
-// Create(name string) (*File, error)
-    wrx := os.Stderr
-    if logfilePtr != nil {
-        if fp, erx := os.Create(*logfilePtr); erx == nil {
-            wrx = fp
-        }
-    }
-    logger = log.New(wrx, "", log.LstdFlags | log.Lshortfile)
 
-    if verbosePtr != nil {
-        verbosity = *verbosePtr
-    }
+	wrx := os.Stderr
+	if logfilePtr != nil {
+		if fp, erx := os.Create(*logfilePtr); erx == nil {
+			wrx = fp
+		}
+	}
+	logger = log.New(wrx, "", log.LstdFlags | log.Lshortfile)
+	
+	if verbosePtr != nil {
+		verbosity = *verbosePtr
+	}
 
 	if flag.NArg() < 2 {
 		flag.Usage()
@@ -263,10 +239,7 @@ func main() {
 		}
 	}
 
-	//glog.Info(versionString)
-    logger.Println(versionString)
-
-	// defer glog.Flush()
+	logger.Println(versionString)
 
 	verifyGoOnPath()
 
@@ -289,95 +262,114 @@ func main() {
 	// Each module has to have a generated name, and, per Go's rules,
 	// be put in a directory with that name.
 	shimPkgBase := "instrumented_module_" + hash
-	// [PH] shimPkg := InstrumentationModuleName + "/" + shimPkgBase
 	shimPkg := InstrumentationModuleName
 
 	shimDirectory := filepath.Join(antithesisOutputDirectory, shimPkgBase)
 	shimPath := filepath.Join(shimDirectory, "instrumented_module.go")
 
 	if e := os.MkdirAll(shimDirectory, 0700); e != nil {
-		//glog.Fatalf("Could not create subdirectory for Antithesis shim %s: %v", shimPath, e)
 		logger.Fatalf("Could not create subdirectory for Antithesis shim %s: %v", shimPath, e)
 	}
 
-	// glog.Infof("Instrumenting %s to %s", customerInputDirectory, customerOutputDirectory)
-	logger.Printf("Instrumenting %s to %s", customerInputDirectory, customerOutputDirectory)
+    want_instrumentor := true
+	if want_instrumentor {
+		logger.Printf("Instrumenting %s to %s", customerInputDirectory, customerOutputDirectory)
+    }
 
 	symbolTableFileBaseName := "go-" + hash
 	if *prefixPtr != "" {
 		symbolTableFileBaseName = *prefixPtr + "-" + symbolTableFileBaseName
 	}
 	symbolTableFileName := symbolTableFileBaseName + ".sym.tsv"
-	symbolTableWriter := CreateSymbolTableFile(filepath.Join(symbolsOutputDirectory, symbolTableFileName), symbolTableFileBaseName)
+	if (want_instrumentor) {
+		symbolsPath := filepath.Join(symbolsOutputDirectory, symbolTableFileName)
+		symbolTableWriter := CreateSymbolTableFile(symbolsPath, symbolTableFileBaseName)
+		instrumentor := CreateInstrumentor(customerInputDirectory, shimPkg, symbolTableWriter)
+	}
+
+    // Setup the assertion scanner (used to create the assertion catalog)
+    aSI := NewScanningInfo(verbose > 0, symbolTableFileBaseName)
+
 
 	filesInstrumented := 0
-
-	instrumentor := CreateInstrumentor(customerInputDirectory, shimPkg, symbolTableWriter)
-
+	filesScanned := 0
 	for _, path := range sourceFiles {
-		// glog.Infof("Instrumenting %s", path)
-		logger.Printf("Instrumenting %s", path)
-		previousEdge := instrumentor.CurrentEdge
-		instrumented, e := instrumentor.Instrument(path)
+		if (want_instrumentor) {
+			logger.Printf("Instrumenting %s", path)
+			previousEdge := instrumentor.CurrentEdge
+			instrumented, e := instrumentor.Instrument(path)
 
-		if e != nil {
-			// glog.Errorf("File %s produced error %s; simply copying source", path, e)
-			logger.Printf("\u274c File %s produced error %s; simply copying source", path, e)
-			continue
+			if e != nil {
+				logger.Printf("\u274c File %s produced error %s; simply copying source", path, e)
+				continue
+			}
+
+			if instrumented == "" {
+				// The instrumentor should have reported why it didn't instrument this file.
+				continue
+			}
 		}
 
-		if instrumented == "" {
-			// The instrumentor should have reported why it didn't instrument this file.
-			continue
-		}
-
-		// Strip the prefix from the input file name. We could also use strings.Rel(),
-		// but we've got absolute paths, so this will work.
-		outputPath := filepath.Join(customerOutputDirectory, path[len(customerInputDirectory):])
-		outputSubdirectory := filepath.Dir(outputPath)
-		os.MkdirAll(outputSubdirectory, 0755)
-
-		// glog.V(1).Infof("Writing instrumented file %s with edges %d–%d", outputPath, previousEdge, instrumentor.CurrentEdge)
-        if verbose_level(1) {
-            logger.Printf("Writing instrumented file %s with edges %d–%d", outputPath, previousEdge, instrumentor.CurrentEdge)
+		if (want_assertion_catalog) {
+			// Scan for assertions
+			logger.Printf("Scanning %s", path)
+			aSI.ScanFile(path)
+			filesScanned++
         }
 
-		if e = writeInstrumentedSource(instrumented, outputPath); e == nil {
-			filesInstrumented++
+		if (want_instrumentor) {
+			// Strip the prefix from the input file name. We could also use strings.Rel(),
+			// but we've got absolute paths, so this will work.
+			outputPath := filepath.Join(customerOutputDirectory, path[len(customerInputDirectory):])
+			outputSubdirectory := filepath.Dir(outputPath)
+			os.MkdirAll(outputSubdirectory, 0755)
+
+			if verbose_level(1) {
+				logger.Printf("Writing instrumented file %s with edges %d–%d", outputPath, previousEdge, instrumentor.CurrentEdge)
+			}
+
+			if e = writeInstrumentedSource(instrumented, outputPath); e == nil {
+				filesInstrumented++
+			}
 		}
 	}
 
-	if err := symbolTableWriter.Close(); err != nil {
-		//glog.Errorf("Could not close symbol table %s: %s", symbolTableWriter.Path, err)
-		logger.Printf("\u274c Could not close symbol table %s: %s", symbolTableWriter.Path, err)
+	if (want_instrumentor) {
+		if err := symbolTableWriter.Close(); err != nil {
+			logger.Printf("\u274c Could not close symbol table %s: %s", symbolTableWriter.Path, err)
+		}
+		logger.Printf("Wrote symbol table %s", symbolTableWriter.Path)
+
+        // TODO: Shim should be added into the file containing the Assertion catalog
+		// Make sure this works when there are no assertions whatsoever
+		writeShimSource(instrumentor.CurrentEdge, shimPkgBase, symbolTableFileName, shimPath)
+		logger.Printf("Antithesis instrumentation shim written to %s", shimPath)
+
+        // TODO: Dont need these wrapper files anymore
+		copyRecursiveNoClobber(*antithesisPtr, antithesisOutputDirectory)
+		logger.Printf("Antithesis instrumentation module %s copied to %s", *antithesisPtr, antithesisOutputDirectory)
+
+        // TODO: Dependencies will just be the Antithesis SDK
+		addDependencies(customerInputDirectory, antithesisOutputDirectory, customerOutputDirectory)
+		logger.Printf("Antithesis dependencies added to %s/go.mod", customerOutputDirectory)
+
+		copyRecursiveNoClobber(customerInputDirectory, customerOutputDirectory)
+		logger.Printf("All other files copied unmodified from %s to %s", customerInputDirectory, customerOutputDirectory)
+
+        num_files_read := len(sourceFiles)
+        num_files_skipped := num_files_read - filesInstrumented
+		logger.Printf("\u261d %d .go files read, %d files skipped, %d edges instrumented", 
+			num_files_read, 
+			num_files_skipped, 
+			instrumentor.CurrentEdge)
 	}
-	//glog.Infof("Wrote symbol table %s", symbolTableWriter.Path)
-	logger.Printf("Wrote symbol table %s", symbolTableWriter.Path)
-
-	writeShimSource(instrumentor.CurrentEdge, shimPkgBase, symbolTableFileName, shimPath)
-	// glog.Infof("Antithesis instrumentation shim written to %s", shimPath)
-	logger.Printf("Antithesis instrumentation shim written to %s", shimPath)
-
-	copyRecursiveNoClobber(*antithesisPtr, antithesisOutputDirectory)
-	// glog.Infof("Antithesis instrumentation module %s copied to %s", *antithesisPtr, antithesisOutputDirectory)
-	logger.Printf("Antithesis instrumentation module %s copied to %s", *antithesisPtr, antithesisOutputDirectory)
-
-	addDependencies(customerInputDirectory, antithesisOutputDirectory, customerOutputDirectory)
-	// glog.Infof("Antithesis dependencies added to %s/go.mod", customerOutputDirectory)
-	logger.Printf("Antithesis dependencies added to %s/go.mod", customerOutputDirectory)
-
-	copyRecursiveNoClobber(customerInputDirectory, customerOutputDirectory)
-	// glog.Infof("All other files copied unmodified from %s to %s", customerInputDirectory, customerOutputDirectory)
-	logger.Printf("All other files copied unmodified from %s to %s", customerInputDirectory, customerOutputDirectory)
-
-	// glog.Warningf("%d .go files read, %d files skipped, %d edges instrumented", len(sourceFiles), len(sourceFiles)-filesInstrumented, instrumentor.CurrentEdge)
-	logger.Printf("\u261d %d .go files read, %d files skipped, %d edges instrumented", len(sourceFiles), len(sourceFiles)-filesInstrumented, instrumentor.CurrentEdge)
+	aSI.WriteAssertionCatalog(ANTITHESIS_SDK_PACKAGE)
+	logger.Printf("\u261d %d files scanned", filesScanned)
 }
 
 func writeShimSource(currentEdge int, shimPkg string, symbolTable string, shimPath string) {
 	f, err := os.Create(shimPath)
 	if err != nil {
-		// glog.Fatalf("Could not open wrapper file %s: %v", shimPath, err)
 		logger.Fatalf("Could not open wrapper file %s: %v", shimPath, err)
 	}
 	defer f.Close()
@@ -390,7 +382,6 @@ func copyFile(sourcePath string, destinationPath string) {
 	inputBytes, e := ioutil.ReadFile(sourcePath)
 	e = ioutil.WriteFile(destinationPath, inputBytes, 0644)
 	if e != nil {
-		// glog.Errorf("Error creating %s: %v", destinationPath, e)
 		logger.Printf("\u274c creating %s: %v", destinationPath, e)
 	}
 }
