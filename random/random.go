537596e0c53cb326efb6064cb13e3552e09dee27
Kevin Hawickhorst <kevin.hawickhorst@antithesis.com>
2024-05-06 12:08:38 -0400

    Fixing typos

--- random/random.go
+++ random/random.go
@@ -5,6 +5,7 @@
 // These functions should not be used to seed a conventional PRNG, and should not have their return values stored and used to make a decision at a later time. Doing either of these things makes it much harder for the Antithesis platform to control the history of your program's execution, and also makes it harder for Antithesis to learn which inputs provided at which times are most fruitful. Instead, you should call a function from the random package every time your program or [workload] needs to make a decision, at the moment that you need to make the decision.
 //
 // These functions are also safe to call outside the Antithesis environment, where they will fall back on values from [crypto/rand].
+//
 // [Antithesis Go SDK]: https://antithesis.com/docs/using_antithesis/sdk/go_sdk.html
 // [Antithesis platform]: https://antithesis.com
 // [workload]: https://antithesis.com/docs/getting_started/workload.html
