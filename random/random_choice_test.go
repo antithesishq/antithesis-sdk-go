a83187474874bbcda9ddfc859f19035b081938e8
Marco Primi <marco@mpri.me>
2024-07-24 16:18:01 -0700

    Add generic version of RandomChoice
    Motivation: RandomChoice does not accept collections of integers, 
    strings, and other non-interface types

--- random/random_choice_test.go
+++ random/random_choice_test.go
@@ -31,3 +31,30 @@ func TestRandomChoice(t *testing.T) {
 		}
 	}
 }
+
+func TestRandomChoiceGeneric(t *testing.T) {
+
+	choices := []string{
+		"Hello",
+		"World",
+		"How",
+		"Are",
+		"You",
+		"?",
+	}
+
+	counts := make(map[string]int)
+	const N = 100
+	for i := 0; i < N; i++ {
+		chosen := RandomChoiceG(choices)
+		counts[chosen] += 1
+	}
+
+	for i, s := range choices {
+		count, present := counts[s]
+		t.Logf("Item %d/%d was chosen %d times", i+1, len(choices), count)
+		if !present {
+			t.Fatalf("Some element was never chosen in %d random choices!", N)
+		}
+	}
+}
